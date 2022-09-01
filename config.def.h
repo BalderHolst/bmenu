9b9d7743a793b5f5227e0fee7b946f23309825b1
Balder W. Holst <balderwh@gmail.com>
2022-09-01 22:27:30 +0200

    added fuzzy find

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -13,6 +14,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#282828" },
 	[SchemeSel] = { "#222222", "#98971a" },
+	[SchemeSelHighlight] = { "#ffc978", "#98971a" },
+	[SchemeNormHighlight] = { "#ffc978", "#282828" },
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -9,10 +10,13 @@ static const char *fonts[] = {
 	"System San Francisco:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeNorm] = { "#ebdbb2", "#282828"},
 	[SchemeSel] = { "#222222", "#98971a" },
+	[SchemeSelHighlight] = { "#ffc978", "#98971a" },
+	[SchemeNormHighlight] = { "#98971a", "#282828" },
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
